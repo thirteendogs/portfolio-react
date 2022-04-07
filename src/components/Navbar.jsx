4b4fe696fb0c0fd1ab1784f36db56fdd262436a0
Daniel <daniel.nova.girbau@gmail.com>
2022-04-07 03:47:43 +0200

    Feature: first iteration to about page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
 import { FaWolfPackBattalion, FaBars, FaTimes } from 'react-icons/fa'
 
 const Navbar = () => {
@@ -20,12 +19,13 @@ const Navbar = () => {
   })
 
   return (
-    <header>
+    <header id='home-page'>
       <nav aria-label='Main navigation' className='navbar'>
         <div className='navbar__container'>
-          <Link to='/' className='navbar__logo'>
+
+          <a href='#home-page' className='navbar__logo'>
             <FaWolfPackBattalion className='navbar__icon' />
-          </Link>
+          </a>
 
           <button className='navbar__menu__icon' onClick={handleNavToggle}>
             {click ? <FaTimes /> : <FaBars />}
@@ -33,16 +33,16 @@ const Navbar = () => {
 
           <ul className='navbar__menu' data-visible={click ? 'true' : 'false'}>
             <li className='navbar__menu__item'>
-              <Link to='/about' className='navbar__menu__link'>ABOUT</Link>
+              <a href='#about-page' className='navbar__menu__link'>ABOUT</a>
             </li>
             <li className='navbar__menu__item'>
-              <Link to='/projects' className='navbar__menu__link'>PROJECTS</Link>
+              <a href='#projects-page' className='navbar__menu__link'>PROJECTS</a>
             </li>
             <li className='navbar__menu__item'>
-              <Link to='/work' className='navbar__menu__link'>WORK</Link>
+              <a href='#work-page' className='navbar__menu__link'>WORK</a>
             </li>
             <li className='navbar__menu__item'>
-              <Link to='/contact' className='navbar__menu__link'>CONTACT</Link>
+              <a href='#contact-page' className='navbar__menu__link'>CONTACT</a>
             </li>
           </ul>
 
