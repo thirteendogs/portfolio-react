e8731e7fd9ede9943ee02ecad4a6d65e1f0d9adb
Daniel <daniel.nova.girbau@gmail.com>
2022-04-10 20:39:12 +0200

    Feature: Projects page and testing

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <h1 className='about__title'>A little bit of me</h1>
         <p className='about__paragraph'>I am Dani and I am from Spain. </p>
         <p className='about__paragraph'>I like to dig in and do my research on how things are made, especially websites and apps.</p>
-        <p className='about__paragraph'>I got into programming by coincidence. While I was happily working in a grocery store, my brother sign me up for an exam to join a programming school. I passed the exam, and since I started school I couldn't stop looking for how things were made.
+        <p className='about__paragraph'>I got into programming by coincidence. While I was happily working in a grocery store, my brother sign me up for an exam to join a programming school. I passed the exam, and since I started school I couldn&apos;t stop looking for how things were made.
         </p>
         <p className='about__paragraph'>After I finished my studies and did my practices at
           <a className='about__company'> Everis</a>, I got hired in <a className='about__company' href=''>Indra </a>
--- src/components/pages/Home.test.js
+++ src/components/pages/Home.test.js
@@ -1,23 +1,23 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
 import Home from './Home'
 
 describe('Home component', () => {
-   beforeAll(() => {
-      render(<Home />)
-   })
+  beforeAll(() => {
+    render(<Home />)
+  })
 
-   test('Home page text are correct', () => {
-      const firstParagraph = screen.getAllByText('Hello! my name is')
-      const firstHeader = screen.getAllByText('Daniel Nova Girbau.')
-      const secondHeader = screen.getAllByText('I am a Front End Developer.')
-      const thirthHeader = screen.getAllByText('I enjoy building and designing cool websites and apps.')
-      const anchorResume = screen.getAllByText('Download CV')
+  test('Home page text are correct', () => {
+    const firstParagraph = screen.getAllByText('Hello! my name is')
+    const firstHeader = screen.getAllByText('Daniel Nova Girbau.')
+    const secondHeader = screen.getAllByText('I am a Front End Developer.')
+    const thirthHeader = screen.getAllByText('I enjoy building and designing cool websites and apps.')
+    const anchorResume = screen.getAllByText('Download CV')
 
-      expect(firstParagraph).toBeDefined()
-      expect(firstHeader).toBeDefined()
-      expect(secondHeader).toBeDefined()
-      expect(thirthHeader).toBeDefined()
-      expect(anchorResume).toBeDefined()
-   })
+    expect(firstParagraph).toBeDefined()
+    expect(firstHeader).toBeDefined()
+    expect(secondHeader).toBeDefined()
+    expect(thirthHeader).toBeDefined()
+    expect(anchorResume).toBeDefined()
+  })
 })
