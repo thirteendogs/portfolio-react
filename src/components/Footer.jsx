0bc82d23badab4c42b679218c703e0809c40dfd0
Daniel <daniel.nova.girbau@gmail.com>
2022-04-12 22:06:56 +0200

    feature: Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <footer className='footer'>
+      <p className='footer__text'>Created by Daniel Aka Thirteendogs 🙃</p>
+   </footer>
+  )
+}
+
+export default Footer
