8787488c80710b355a03f88527d66c2b01934178
Daniel <daniel.nova.girbau@gmail.com>
2022-04-23 00:50:21 +0200

    add socials link on smaller screens

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -8,7 +8,22 @@ const Home = () => {
         <h1 className='home__secondlinetitle'>Daniel Nova Girbau.</h1>
         <h2 className='home__thirthlinetitle'>I am a Front End Developer.</h2>
         <h3 className='home__fourthlinetitle'>I enjoy building and designing cool websites and apps.</h3>
-        <a href='./assets/Resume Daniel Nova Girbau.pdf' className='btn' download>Download CV</a>
+        <div className="home__socials">
+          <div className="home__socials__resume">
+          <a href='./assets/Resume Daniel Nova Girbau.pdf' className='btn' download>Download CV</a>
+          </div>
+
+            <ul className="home__socials__link">
+              <li>
+                <a href="https://github.com/thirteendogs/" target="_blank" rel="noreferrer"><i className="fa fa-github"></i></a>
+              </li>
+              <li>
+                <a className="side-line" href="https://www.linkedin.com/in/danielnovagirbau/" target="_blank" rel="noreferrer"><i
+                      className="fa fa-linkedin"></i></a>
+              </li>
+            </ul>
+
+        </div>
       </div>
       <div className='button__setup'>
         <a className='button' href='#about-page' />
