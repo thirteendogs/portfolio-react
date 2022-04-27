43ba7740848f47b7c0abc1202b7f32b5c2fddbea
Daniel <daniel.nova.girbau@gmail.com>
2022-04-27 20:11:11 +0200

    fix loading intro page

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,15 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { FaWolfPackBattalion } from 'react-icons/fa'
 
 const Intro = () => {
-  const [show, setShow] = useState(true)
-
-  setTimeout(() => {
-    setShow(false)
-  }, 4000)
-
   return (
-    <div className='intro' data-show={`${show ? 'true' : 'false'}`}>
+    <div className='intro'>
       <div className="intro__box__one"></div>
       <div className="intro__box__two"></div>
        <FaWolfPackBattalion className='intro__icon'/>
